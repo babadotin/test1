5bd0ac5bc46f96d1c749a62fb38a3807341d9fa7
Baba.in <shubhamgoyalk567@gmail.com>
2024-06-19 13:04:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+// Basic Dart questions-for-practice
+// https://dart-tutorial.com/introduction-and-basics/questions-for-practice-1/
+
+import 'package:flutter/material.dart';
+// 1,  Write a program to debugPrint your name in Dart
+
+void main() {
+  debugPrint("Shubham");
+}
+
+
+// 2,  Write a program to debugPrint Hello I am “Baba” and Hello I’am “Baba” with single and double quotes
+// void main() {
+//   String name = 'baba';
+//   debugPrint('Hello I am "$name"');
+//   debugPrint("Hello I am '$name'");
+// }
+
+// 3,  Declare constant type of int set value 7
+// void main() {
+//   const int value = 7;
+//   debugPrint('$value');
+//   }
+
+// 4,  Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+// void main() {
+//   int a = 2000; // amount
+//   int t = 5; // time
+//   int i = 5; // intrest
+//   double formula = (a * t * i) / 100;
+//   debugPrint('$formula');
+// }
+
+// 5,  Write a program to debugPrint a square of a number using user input.
+// void main() {
+//   int num = 9;
+//   int square = num * num;
+//   debugPrint('$square');
+// }
+
+// 6,  Write a program to debugPrint full name of a from first name and last name using user input.
+// void main() {
+//   String firstname = 'Shubham';
+//   String scndname = 'Baba';
+//   String fullname = firstname + scndname;
+//   debugPrint(fullname);
+// }
+
+// 7, Write a program to find quotient and remainder of two integers.
+// void main() {
+//   int firstInt = 10;
+//   int scndInt = 4;
+//   int quotient = (firstInt ~/ scndInt);
+//   int remainder = (firstInt % scndInt);
+//   debugPrint('$quotient');
+//   debugPrint('$remainder');
+// }
+
+// 8, Write a program to swap two numbers
+// void main() {
+//   int num1 = 5;
+//   int num2 = 10;
+
+//   int num3 = num1;
+//   num1 = num2;
+//   num2 = num3;
+
+//   debugPrint('After swapping: num1 = $num1, num2 = $num2');
+// }
